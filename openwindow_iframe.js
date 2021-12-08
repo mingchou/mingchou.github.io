9f50438da41b912547e1f8a80f16eddc41953fcd
stone <stone.shih@kaiostech.com>
2021-12-08 16:54:05 +0800

    add window open in iframe test

---
+++ openwindow_iframe.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.opener').forEach((btn => {
+    btn.addEventListener('click', (evt) => {
+      const targetId = `${evt.target.dataset.target}`;
+      window.popupWindow = window.open('openwindow_iframe.html', targetId);
+      window.popupWindow.postMessage("aaaaaaaaaaaaaaaaaaaaaaaaaaa", location.origin);
+    });
+  }));
+  
+  document.querySelectorAll('.noopener').forEach((btn => {
+    btn.addEventListener('click', (evt) => {
+      const targetId = `${evt.target.dataset.target}`;
+      window.popupWindow = window.open('openwindow_iframe.html', targetId, 'noopener');
+      window.popupWindow.postMessage("aaaaaaaaaaaaaaaaaaaaaaaaaaa", location.origin);
+    });
+  }));
+
+  document.querySelectorAll('.noreferrer').forEach((btn => {
+    btn.addEventListener('click', (evt) => {
+      const targetId = `${evt.target.dataset.target}`;
+      window.popupWindow = window.open('openwindow_iframe.html', targetId, 'noreferrer');
+      window.popupWindow.postMessage("aaaaaaaaaaaaaaaaaaaaaaaaaaa", location.origin);
+    });
+  }));
+
+  window.addEventListener('message', (event) => {
+    console.log(`Received message: ${event.data}`);
+  });
+  
